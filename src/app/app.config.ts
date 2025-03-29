642d468a7dba78f7a8951d4921a47dd4eb0d5cfe
DITTRICH ÁKOS <Akos2.Dittrich@kh.hu>
2025-03-29 20:59:32 +0100

    add basic login redirect to keycloak

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,38 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { KeycloakService, KeycloakAngularModule } from 'keycloak-angular';
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  console.log('Initializing Keycloak...');
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost',
+        realm: 'dormnet',
+        clientId: 'dormnet-api',
+      },
+      initOptions: {
+        onLoad: 'login-required',
+        checkLoginIframe: false,
+        flow: "standard"
+      },
+    });
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    KeycloakService,
+    KeycloakAngularModule,
+    {
+      provide:
+      APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      deps: [KeycloakService],
+      multi: true,
+    }
+  ]
 };
